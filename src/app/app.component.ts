aa7b36f1ec365f39766261f2edfd205d3ea2c589
simao2018 <chancelsim@outlook.fr>
2022-02-06 22:29:38 +0100

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stripe-angular';
+
+  paymentHandler: any = null;
+  constructor() { }
+  ngOnInit() {
+    this.invokeStripe();
+  }
+
+  makePayment(amount: number) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51KQDJ5EYBgDFmULM7JmceeJzBDMfENSGSFyuk4MWQLohLs0Tvq9ExGY1tZslTOncEyp9if23l43svpV2TxXg3vAq00D4QkLKZE',
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken)
+        alert('Stripe token generated!');
+      }
+    });
+
+    paymentHandler.open({
+      name: 'Positronx',
+      description: '3 widgets',
+      amount: amount * 100
+    });
+  }
+
+  invokeStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement("script");
+      script.id = "stripe-script";
+      script.type = "text/javascript";
+      script.src = "https://checkout.stripe.com/checkout.js";
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51KQDJ5EYBgDFmULM7JmceeJzBDMfENSGSFyuk4MWQLohLs0Tvq9ExGY1tZslTOncEyp9if23l43svpV2TxXg3vAq00D4QkLKZE',
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken)
+            alert('Payment has been successfull!');
+          }
+        });
+      }
+
+      window.document.body.appendChild(script);
+    }
+  }
 }
